1f191d9f2a767daa542eb5be214542b01f0bcb81
Stephen P. Potter <spp@unixsa.net>
2021-02-14 18:00:40 +1100

    initial commit

---
+++ story.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+printf "\033c"
+echo "This is the story game. We will make a funny story..."
+sleep 6
+printf "\033c"
+echo "Don't type anything until I ask for it please..."
+sleep 6
+printf "\033c"
+echo "OK ?"
+sleep 6
+echo "GREAT!!! :D"
+sleep 3
+printf "\033c"
+echo "Let's get started then."
